aa1b752f2c959f2058fb253f2ff9f35efe019019
Alexis Hennecy <alexishennecy@users-MacBook-Pro-2.local>
2018-03-06 11:54:54 -0600

    fixed jquery in gruntfile and package.json, installed jquery locally

--- ajaxCall.js
+++ ajaxCall.js
@@ -1,7 +1,5 @@
 "use strict";
-
-let $ = require('../lib/node_modules/jquery');
-console.log("main js here");
+let $ = require('jquery');
 
 let meetUps = "https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=10&page=10&key=742a785d27225b161f1072604e5e3a27";
 
@@ -14,7 +12,7 @@ function getMeetup(data){
     })
     .then((data) => {
         return data;
-    });
+    })
 }
 getMeetup(meetUps);
 console.log(data);
