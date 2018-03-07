08c3345491ad8ec1b2d761e6c6557fb681f042da
Phonethip <phonethip.liu@gmail.com>
2018-03-07 15:12:47 -0600

    got top 10 news articles to output to the dom

--- js/news-Getter.js
+++ js/news-Getter.js
@@ -5,17 +5,35 @@ console.log("News API data in the house");
 
 let $ = require('jquery');
 
-var url = "https://newsapi.org/v2/top-headlines?country=us&";
-// var newsApiKey = "511e64b5fdc44764af3517769a250375";
+let news = {};
 
+var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=511e64b5fdc44764af3517769a250375&limit=10";
+// var newsApiKey = "511e64b5fdc44764af3517769a250375&limit=10";
 
-let news = [];
-let api_calls = {};
+var newsDiv = document.getElementById("news--div");
+console.log("targeting news div in dom", newsDiv);
 
-//  PH == jQuery xhr option works w/jquery cdn script in html \\
+//jQuery to put everything in an array
+var populateNewsDiv = $("#news--list");
+console.log("what is in the populateNewDiv jquery", populateNewsDiv[0]);
 
-function outputNews (){
-    console.
+function outputNews(newsList) {
+    console.log("what is this newArticle in the outputNews function", newsList);
+    
+    //parameter to make it runs if there is a newsArticles
+    if (newsList.articles) {
+        for (let i = 0; i < 10; i++) {
+            var articles= newsList.articles;
+            console.log("topNewsArticle", newsList);
+            console.log("News data title:", articles[i].title + articles[i].description  + articles[i].source + articles[i].url );
+            populateNewsDiv.append(`<li class="list">
+            <h4 class="news-headline">${articles[i].title}</h4>
+            <p>${articles[i].description}</p>
+            <footer class="news-footer"> Source: ${articles[i].source.name}</footer>
+            <a target="_blank" href="${articles[i].url}"> Read the article </a></li>`);
+            
+        }
+    }
 }
 
 
@@ -28,14 +46,12 @@ function newsAPICall(url) {
 
 news = newsAPICall(url)
 .then ((resolve) => {
-    console.log("makeAPICall for top News Resolved", articles['0'].title); 
-
-    // function outputNews(); // I want it to do this function
+    console.log("makeAPICall for top News Resolved", resolve); 
+    outputNews(resolve); // I want it to do this function
     },
     (reject) => {
         console.log("DOH! something went wrong");
     }
 );
-
 
 module.exports = { news };
