eea99b16b6edd29f75d9c269be6e1cb5eb8b1bed
Brittany D. Smith <bdaniphotography@gmail.com>
2018-03-08 15:45:16 -0600

    Done fooling with book search

--- js/books-setter.js
+++ js/books-setter.js
@@ -15,7 +15,7 @@ search.captureInput = () => {
     // Takes user input and makes it lowercase
             let userInput = e.target.value.toLowerCase();
             // Console logs whatever the user types in
-            console.log(userInput);
+            console.log("User input for book search:", userInput);
             bookSearch(userInput);
         // If the "enter" key is pressed, but there is no value entered, an alert is triggered
         } else if (e.keyCode === 13) {
@@ -37,17 +37,17 @@ let bookSearch = (userInput) => {
 // This builds the list of books with their title, author, and the published year
 let bookDisplay = (arrayBooks) => {
     // Console logs the array of books returned from the search
-    console.log(arrayBooks);
+    console.log("Returned array of book search results", arrayBooks);
     let searchResult = "";
     for (let i = 0; i < arrayBooks.length; i++) {
-        if (arrayBooks[i].isbn) {
-            let bookThumbnail = arrayBooks[i].isbn[0];
-            console.log("bookThumbnail", bookThumbnail);
-            searchResult += `<img src="http://covers.openlibrary.org/b/isbn/${bookThumbnail}-S.jpg">`;
-        }
-        searchResult += `<h4>${arrayBooks[i].title}</h4>`;
-        searchResult += `<h5 class="lead text-info">By: ${arrayBooks[i].author_name}</h5>`;
-        searchResult += `<p>Published: ${arrayBooks[i].first_publish_year}</p>`;
+        // Adds thumbnail image of book cover; takes too long to load & sometimes returns no image at all
+        // if (arrayBooks[i].isbn) {
+        //     let bookThumbnail = arrayBooks[i].isbn[0];
+        //     searchResult += `<img src="http://covers.openlibrary.org/b/isbn/${bookThumbnail}-S.jpg" alt="book cover thumbnail image">`;
+        // }
+        searchResult += `<h4 class="list-headline">${arrayBooks[i].title}</h4>`;
+        searchResult += `<p class="list-summary">By: ${arrayBooks[i].author_name}</p>`;
+        searchResult += `<footer class="list-footer">Published: ${arrayBooks[i].first_publish_year}</footer>`;
         searchResult += `<button id="save--article--btn" type="button" class="btn btn-light btn-sm" data-toggle="button" aria-pressed="false" autocomplete="off" target="my--btn--news">Add to Bookshelf</button>`;
     }
     // Grabs the empty <div> from index.html with the ID of "content" and fills it with "newContent"
