b4261a9c01f575d386acc8d895ae54dc6713b975
Phonethip <phonethip.liu@gmail.com>
2018-03-08 01:46:43 -0600

    worked on update of css for div list styling

--- js/news-Getter.js
+++ js/news-Getter.js
@@ -1,9 +1,9 @@
 "use strict";
 
-
 console.log("News API data in the house");
 
 let $ = require('jquery');
+// let outputNews = require('./news-Setter');
 
 let news = {};
 
@@ -11,27 +11,26 @@ var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=511e64b5fdc447
 // var newsApiKey = "511e64b5fdc44764af3517769a250375&limit=10";
 
 var newsDiv = document.getElementById("news--div");
-console.log("targeting news div in dom", newsDiv);
+// console.log("targeting news div in dom", newsDiv);
 
 //jQuery to put everything in an array
 var populateNewsDiv = $("#news--list");
-console.log("what is in the populateNewDiv jquery", populateNewsDiv[0]);
+// console.log("what is in the populateNewDiv jquery", populateNewsDiv[0]);
 
 function outputNews(newsList) {
-    console.log("what is this newArticle in the outputNews function", newsList);
+    // console.log("what is this newArticle in the outputNews function", newsList);
     
     //parameter to make it runs if there is a newsArticles
     if (newsList.articles) {
         for (let i = 0; i < 10; i++) {
             var articles= newsList.articles;
-            console.log("topNewsArticle", newsList);
-            console.log("News data title:", articles[i].title + articles[i].description  + articles[i].source + articles[i].url );
+            // console.log("topNewsArticle", newsList);
+            // console.log("News data title:", articles[i].title + articles[i].description  + articles[i].source + articles[i].url );
             populateNewsDiv.append(`<li>
-            <h4 class="list-headline">${articles[i].title}</h4>
-            <p>${articles[i].description}</p>
+            <h4 class="list-headline"><a class="list-link" target="_blank" href="${articles[i].url}">${articles[i].title} </a> </h4>
+            <p class="list-summary">${articles[i].description} </p>
             <footer class="list-footer"> Source: ${articles[i].source.name}</footer>
-            <a class="news-link" target="_blank" href="${articles[i].url}"> Read the article </a></li>
-            <button event="onClick()"> add </button>`);
+            <button id="save--article--btn" type="button" class="btn btn-light btn-sm" data-toggle="button" aria-pressed="false" autocomplete="off" target="my--btn--news"> save article </button> </li>`);
             
         }
     }
@@ -45,6 +44,7 @@ function newsAPICall(url) {
     });
 }
 
+
 news = newsAPICall(url)
 .then ((resolve) => {
     console.log("makeAPICall for top News Resolved", resolve); 
@@ -55,4 +55,5 @@ news = newsAPICall(url)
     }
 );
 
-module.exports = { news };
+
+module.exports = newsAPICall(url);
