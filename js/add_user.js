c4f8d2cee78cbfbb77c1beb87cc6b58c5acab7eb
Million Yemane <wedi_ngus@yahoo.com>
2018-03-08 09:58:25 -0600

    lala

--- js/add_user.js
+++ js/add_user.js
@@ -1,27 +1,27 @@
 "use strict"
 
 
-var $= require('jquery');
-var news = require("./news-Getter");
-var meetup = require('./ajaxCall');
-var bookSearch = require("./books-getter");
-var weather = require("./weather_load");
-var user = require("./user");
+// var $= require('jquery');
+// var news = require("./news-Getter");
+// var meetup = require('./ajaxCall');
+// var bookSearch = require("./books-getter");
+// var weather = require("./weather_load");
+// var user = require("./user");
 
-let $ = require('jquery'),
-    firebase = require("./config"),
-    user = require("./user"),
-    db = require("./apiKeys");
+// let $ = require('jquery'),
+//     firebase = require("./config"),
+//     user = require("./user"),
+//     db = require("./apiKeys");
 
-function addUser(userObj) {
-    // console.log("addUser", userObj);
-    return $.ajax({
-        url: `${firebase.getFBsettings().databaseURL}/users.json`,
-        type: 'POST',
-        data: JSON.stringify(userObj),
-        dataType: 'json'
-    }).done((userID) => {
-        return userID;
-    });
-}
-module.exports = addUser;
+// function addUser(userObj) {
+//     // console.log("addUser", userObj);
+//     return $.ajax({
+//         url: `${firebase.getFBsettings().databaseURL}/users.json`,
+//         type: 'POST',
+//         data: JSON.stringify(userObj),
+//         dataType: 'json'
+//     }).done((userID) => {
+//         return userID;
+//     });
+// }
+// module.exports = addUser;
