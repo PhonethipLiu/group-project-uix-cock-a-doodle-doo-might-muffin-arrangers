6e29bf11efda77bceefe51db3829b67446cc18e2
Phonethip <phonethip.liu@gmail.com>
2018-03-08 15:50:07 -0600

    wrote functions for firebase news interactions

--- js/news-interactions.js
+++ js/news-interactions.js
@@ -8,7 +8,7 @@ firebase = require("./config");
 // function getNews(user) {
 //     console.log("url", firebase.getFBsettings().databaseURL);
 //     return $.ajax ({
-//         url: `${firebase.getFBsettings().databaseURL}/songs.json?orderBy="uid"&equalTo="${user}"`
+//         url: `${firebase.getFBsettings().databaseURL}/news.json?orderBy="uid"&equalTo="${user}"`
 //     }).done( (getNewsData) => {
 //         console.log("this function in promise", getNewsData);
 //         return getNewsData;
@@ -18,19 +18,63 @@ firebase = require("./config");
 // **** this version is without user id ****** //
 function getNews() {
     return $.ajax ({
-        url: `${firebase.getFBsettings().databaseURL}/xxxx.json`
+        url: `${firebase.getFBsettings().databaseURL}/news.json`
     }).done( (getNewsData) => {
         console.log("this function in promise", getNewsData);
         return getNewsData;
     });
 }
 
-function editMyNews(newsFormObj) {
+function addSaveNews(newsFormObj) {
     console.log("editMyNews", newsFormObj);
     return $.ajax({
-        url:`${firebase.getFBsettings().databaseURL}/xxxx.json`,
+        url:`${firebase.getFBsettings().databaseURL}/news.json`,
         type: "POST",
         data: JSON.stringify(newsFormObj),
         dataType: "json"
+    }).done( (newsId) => {
+        console.log("what is newsID:", newsId);
+        return newsId;
+    });
+   
+}
+
+function deleteSaveNews(newsId){
+    $.ajax({
+        url: `${firebase.getFBsettings().databaseURL}/news/${newsId}.json`,
+        method: "DELETE"
+    }).done ( (newsData) => {
+        console.log("what is newsData:", newsData);
+        return newsData;
     });
 }
+
+function getMyNews(newsId){
+    $.ajax({
+        url: `${firebase.getFBsettings().databaseURL}/news/${newsId}.json`,
+    }).done ( (newsData) => {
+        console.log("what is newsData:", newsData);
+        return newsData;
+    }).fail( (error) => {
+        return error;
+    });
+}
+
+function editMyNews(newsFormObj, newsId) {
+    return $.ajax({
+        url: `${firebase.getFBsettings().databaseURL}/news/${newsId}.json`,
+        type: "PUT",
+        data: JSON.stringify(newsFormObj)
+    }).done( (newsdata) => {
+        return newsdata;
+    });
+}
+
+module.exports = { 
+    getNews, 
+    addSaveNews, 
+    deleteSaveNews, 
+    getMyNews, 
+    editMyNews
+};
+
