ee74665561af2e9b54754cbe338f27c62a1b2448
Dan Watson <danwatson00@yahoo.com>
2018-03-08 18:26:32 -0600

    commented out time stuff

--- js/meetups-getter.js
+++ js/meetups-getter.js
@@ -43,4 +43,4 @@ function createMeetupCards (events) {
 
  // url: "https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=10&page=10&key=742a785d27225b161f1072604e5e3a27"
 
-module.exports = { currentMeetupTime, currentMeetupDate, getMeetup, createMeetupCards };
+module.exports = { getMeetup, createMeetupCards };
--- js/time.js
+++ js/time.js
@@ -2,23 +2,23 @@
 
 console.log("Hello time.js");
 
-var meetupDateTime = require("./meetups-getter");
+// var meetupDateTime = require("./meetups-getter");
 
-var currentDateTime = new Date();
-
-
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var currentDateTime = new Date();
 
 
-var currentMonth = monthNames[currentDateTime.getMonth()];
-var currentDate = currentDateTime.getDate();
-var currentYear = currentDateTime.getFullYear();
-var dateToday = currentMonth + " " + currentDate + ", " + currentYear;
+// var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var meetupMonth = monthNames[meetupDateTime.getMonth()];
-var meetupDate = meetupDateTime.getDate();
-var meetupYear = meetupDateTime.getFullYear();
-var meetupStartTime = meetupMonth + " " + meetupDate + ", " + meetupYear;
 
-module.exports = { dateToday, meetupStartTime };
+// var currentMonth = monthNames[currentDateTime.getMonth()];
+// var currentDate = currentDateTime.getDate();
+// var currentYear = currentDateTime.getFullYear();
+// var dateToday = currentMonth + " " + currentDate + ", " + currentYear;
+
+// var meetupMonth = monthNames[meetupDateTime.getMonth()];
+// var meetupDate = meetupDateTime.getDate();
+// var meetupYear = meetupDateTime.getFullYear();
+// var meetupStartTime = meetupMonth + " " + meetupDate + ", " + meetupYear;
+
+// module.exports = { dateToday, meetupStartTime };
 
